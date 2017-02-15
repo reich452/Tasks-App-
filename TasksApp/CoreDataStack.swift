23506f9cc27bb0fc87ac9750aef5d8ba28d6f200
reich452 <reich452@regis.edu>
2017-02-15 14:07:06 -0700

    Completed the core data stack file

---
+++ TasksApp/CoreDataStack.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataStack.swift
+//  TasksApp
+//
+//  Created by Nick Reichard on 2/15/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
+
